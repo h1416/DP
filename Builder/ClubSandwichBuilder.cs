51cc9371f2c14095fd54ec6f14f760c12d5135d3
hcvo <jba152@att.net>
2017-04-21 14:40:23 -0500

    Builder - Added sandwichbuilder, the first version

---
+++ Builder/ClubSandwichBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Builder
+{
+    class ClubSandwichBuilder
+    {
+        Sandwich sandwich;
+
+        public Sandwich GetSandwich()
+        {
+            return sandwich;
+        }
+
+        public void CreateSandwich()
+        {
+            sandwich = new Sandwich();
+            PrepareBread();
+            ApplyMeatAndCheese();
+            ApplyVeggies();
+            AddCondiments();
+        }
+
+        private void AddCondiments()
+        {
+            sandwich.HasMustard = true;
+            sandwich.HasMustard = true;
+        }
+
+        private void ApplyVeggies()
+        {
+            sandwich.Vegetables = new List<string> { "Tomato", "Lettuce" };
+        }
+
+        private void ApplyMeatAndCheese()
+        {
+            sandwich.MeatType = MeatType.Turkey;
+            sandwich.BreadType = BreadType.Wheat;
+        }
+
+        private void PrepareBread()
+        {
+            sandwich.BreadType = BreadType.Wheat;
+            sandwich.IsToasted = true;
+        }
+    }
+}
