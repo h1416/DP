5f83db422c48e3a1fb0eb3058a4920a6bff892d9
hcvo <jba152@att.net>
2017-04-25 08:34:20 -0500

    Command - Added command parser

---
+++ Command/CommandParser.cs
@@ -0,0 +1,30 @@
+﻿using Command.Commands;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Command
+{
+    public class CommandParser
+    {
+        readonly IEnumerable<ICommandFactory> availableCommands;
+
+        public CommandParser(IEnumerable<ICommandFactory> availableCommands)
+        {
+            this.availableCommands = availableCommands;
+        }
+
+        internal ICommand ParseCommand(string[] args)
+        {
+            var requestedCommandName = args[0];
+
+            var command = FindRequestedCommand(requestedCommandName);
+
+            return command.MakeCommand(args);
+        }
+
+        private ICommandFactory FindRequestedCommand(string commandName)
+        {
+            return availableCommands.FirstOrDefault(cmd => cmd.CommandName.Equals(commandName));
+        }
+    }
+}
