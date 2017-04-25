340ef4ad8261ad2f2120d526543baf7732c965bb
hcvo <jba152@att.net>
2017-04-25 08:05:23 -0500

    Command - Added ICommand and the first command class

---
+++ Command/UpdateQuantityCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Command
+{
+    class UpdateQuantityCommand : ICommand
+    {
+        public int NewQuantity { get; set; }
+        public void Execute()
+        {
+            // simulate updating a database
+            const int oldQuantity = 5;
+            Console.WriteLine("DATABASE: Updated");
+
+            // simulate logging
+            Console.WriteLine($"LOG: Updated order quantity from {oldQuantity} to {NewQuantity}");
+        }
+    }
+}
